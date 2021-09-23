caf6f5df9005881955e7eca6770788eb4e09bf3c
sharmag44 <62098467+sharmag44@users.noreply.github.com>
2021-09-24 00:08:40 +0530

    localstorage in added & login  page is working

--- script.js
+++ script.js
@@ -1,66 +1,57 @@
-
-
 //Registration data
 var arr = [];
-var index = 0
+var index = 0;
 function register() {
   let user = document.getElementById("user").value;
   let password = document.getElementById("password").value;
-  let account =parseInt(document.getElementById("account").value);
+  let account = parseInt(document.getElementById("account").value);
   let money = parseInt(document.getElementById("money").value);
-   var data = [user, password, account, money];
+  var data = [user, password, account, money];
   arr[index] = data;
   index++;
-  console.log(arr)
+  localStorage.setItem("myarr", JSON.stringify(arr));
 }
-    
-    //login data
-   function  login() {
-      var login = document.getElementById("login").value;
-      var pass = document.getElementById("pass").value;
-    
-    for(var i = 0; i < arr.length; i++) {
-            if((arr[i][0]===login)&&(arr[i][0]===pass)){
-              window.location.href("./dashboard.html");
-            }
-            else{
-              console.log("Error");
-            }
 
-            }
+//login data
+function getData() {
+  var myarr = localStorage.getItem("myarr");
+  var newArr = JSON.parse(myarr);
+  if ((document.getElementById("user")!= null)&&(document.getElementById("pass")!= null)) {
+    var login = document.getElementById("user").value;
+    var pass = document.getElementById("pass").value;
+      for(let i=0;i<newArr.length;i++){
+        if((newArr[i][0]===login)&&(newArr[i][1]===pass)){
+          alert("Matched")
+        }
+        else{
+          alert("Not Matched")
+        }
+      }
+}
+}
 
+function withdraw() {
+  if (newArr[i][3] !== 0) {
+    let withdrawAmount = parseInt(
+      document.getElementById("Withdraw-money").value
+    );
+   newArr[i][3] = newArr[i][3] - withdrawAmount;
+    alert(newArr[i][3]);
+  }
+}
+function deposit() {
+  let depositAmount = parseInt(document.getElementById("deposit").value);
+  newArr[i][3] + depositAmount;
+  window.location.href("./dashboard.html");
+}
+function check() {
+  alert(newArr[i][3]);
+  window.location.href("./dashboard.html");
+}
+// function updateData(){
 
-              function withdraw(){
-                  if(arr[i][3]!==0){
-                      let withdrawAmount =parseInt(document.getElementById("Withdraw-money").value);
-                      arr[i][3]-withdrawAmount;
-                      document.write(arr[i][3]);
-                      window.location.href("./dashboard.html");
-                  }
-              }
-              function deposit(){
-                
-                    let depositAmount =parseInt(document.getElementById("deposit").value);
-                    arr[i][3]+depositAmount;
-                    window.location.href("./dashboard.html");
-                }
-                function check(){
-                
-                    alert(arr[i][3])
-                    window.location.href("./dashboard.html");
-                }
-                // function updateData(){
+// }
 
-                // }
-            }
-            
-        
-   
-
-    
-    
-    
-    // document.getElementById("withdraw").addEventListener("click",function(){
-    //     if user
-    // })
-
+// document.getElementById("withdraw").addEventListener("click",function(){
+//     if user
+// })
